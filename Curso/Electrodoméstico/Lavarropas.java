476835c019703006829b45ae0ff8ecd85f4d0923
Luis Méndez <44847230+mndzl@users.noreply.github.com>
2019-08-12 19:18:48 -0300

    Create Lavarropas.java

---
+++ Curso/Electrodoméstico/Lavarropas.java
@@ -0,0 +1,26 @@
+package EjercicioElectrodomésticos;
+
+public class Lavarropas extends Electrodoméstico {
+	private int carga;
+	private static final int carga_def = 5;
+	
+	public Lavarropas() {
+		this.carga = carga_def;
+	}
+	public Lavarropas(float precio, float peso) {
+		super(precio,peso);
+	}
+	public Lavarropas(int carga, float precio, String color, char consumo, float peso) {
+		super(precio,color,consumo,peso);
+		this.carga = carga;
+	}
+	public int getCarga() {
+		return this.carga;
+	}
+	
+	@Override
+	public float precioFinal() {
+		if(this.carga>30)precio+=50;
+		return super.precioFinal();
+	}
+}
