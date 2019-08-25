7071ba80f91806d8e2934c9a2f8191e1884e044e
Luis Mendez <mendezgla.56@gmail.com>
2019-08-25 17:12:43 -0300

    MyComplex class added

---
+++ Curso/AvanzadosPOO/MyComplex/MyComplex.java
@@ -0,0 +1,17 @@
+package MyComplex;
+
+public class MyComplex {
+	private double real;
+	private double imag;
+	
+	static final double REAL_DEF = 0.0;
+	static final double IMAG_DEF = 0.0;
+	
+	MyComplex(){
+		this.real = REAL_DEF;
+		this.imag = IMAG_DEF;
+	}
+	
+	
+	
+}
