1d4eda08d8146b11047b309306e662e51a82460c
Luis Mendez <mendezgla.56@gmail.com>
2019-08-25 18:15:00 -0300

    Update MyComplex class

--- Curso/AvanzadosPOO/MyComplex/MyComplex.java
+++ Curso/AvanzadosPOO/MyComplex/MyComplex.java
@@ -7,11 +7,74 @@ public class MyComplex {
 	static final double REAL_DEF = 0.0;
 	static final double IMAG_DEF = 0.0;
 	
-	MyComplex(){
+	public MyComplex(){
 		this.real = REAL_DEF;
 		this.imag = IMAG_DEF;
 	}
+	MyComplex(double real, double imag){
+		this.real = real;
+		this.imag = real;
+	}
 	
+	public double getReal() {
+		return this.real;
+	}
+	public double getImag() {
+		return this.imag;
+	}
+	public void setReal(double real) {
+		this.real = real;
+	}
+	public void setImag(double imag) {
+		this.imag = imag;
+	}
+	public void setValue(double real, double imag) {
+		this.real = real;
+		this.imag = imag;
+	}
 	
+	@Override
+	public String toString() {
+		return "(" + this.real + "+" +  this.imag + "i)";
+	}
 	
+	public boolean isReal() {
+		return this.imag==0;
+	}
+	public boolean isImaginary() {
+		return this.imag!=0;
+	}
+	public boolean equals(double real, double imag){
+		return (this.real == real && this.imag == imag);
+	}
+	public boolean equals(MyComplex another) {
+		return (this.real == another.real && this.imag == another.imag);
+	}
+	public double magnitude() {
+		
+	}
+	public double argument() {
+		
+	}
+	public MyComplex add(MyComplex right) {
+		
+	}
+	public MyComplex addNew(MyComplex right) {
+		
+	}
+	public MyComplex substract(MyComplex right) {
+		
+	}
+	public MyComplex substractNew(MyComplex right) {
+		
+	}
+	public MyComplex multiply(MyComplex right) {
+		
+	}
+	public MyComplex divide(MyComplex right) {
+		
+	}
+	public MyComplex conjugate(MyComplex right) {
+		
+	}
 }
