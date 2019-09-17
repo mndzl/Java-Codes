488c7ea6a970f64ed8c30cebaab4b5f2fa74f5bf
Luis Méndez <44847230+mndzl@users.noreply.github.com>
2019-09-17 19:43:03 -0300

    Create InfinityException.java

---
+++ Curso/Exceptions/InfinityException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package infinity;
+
+/**
+ *
+ * @author Alumno
+ */
+public class InfinityException extends Exception{
+    InfinityException(){super("El numero ingresado es infinito");}
+}
